18d9f4f55d7499ece03f67b1be5800795f180c27
Jason Witzemann <jason.witzemann@students.fhv.at>
2022-06-01 12:30:42 +0200

    Init

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 2022
+
+@author: Christoph, Jason, Tobias 
+"""
+import pandas as pd
+import matplotlib.pyplot as plot
+
+penguins = pd.read_csv("penguins.csv")
+print(penguins)
+
+        
